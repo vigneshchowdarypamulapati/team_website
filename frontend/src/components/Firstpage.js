e312cded5475610b3a1f68db404ec5c4a4b8c136
vigneshchowdarypamulapati <112378529+vigneshchowdarypamulapati@users.noreply.github.com>
2024-12-05 23:51:49 -0700

    Update Firstpage.js

--- frontend/src/components/Firstpage.js
+++ frontend/src/components/Firstpage.js
@@ -5,9 +5,17 @@ export default function Firstpage() {
   return (
     <>
       <div>
-        <Navbar title="NLP Translator"/>
+        <Navbar title="NLP Translator" />
       </div>
       <h1 style={{ textAlign: "center" }}>Welcome to Speech Converter</h1>
+
+      <p style={{ textAlign: "center" }}>
+        Use the Home button to go to Speech to Text Converter page.
+      </p>
+
+      <p style={{ textAlign: "center" }}>
+        Use ChatGPT to interact with AI.
+      </p>
     </>
   );
 }
